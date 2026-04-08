46f997e8b516dd14177452aff13127d479ed6eab
Piotr <pkaleksandrowicz@gmail.com>
2026-04-08 13:27:26 +0200

    update: timer stop & resume

--- lib/Timer.py
+++ lib/Timer.py
@@ -1,24 +1,24 @@
 import datetime as dt
 
-class TimerNotStarted(Exception):
-    def __init__(self, *args):
-        super().__init__("Timer not started yet")
-
 class Timer:
+    _stop_time: dt.datetime = None
+    _count_from_time: dt.datetime = None
+    _time: dt.datetime = None
+
     time: dt.timedelta = dt.timedelta()
     start_time: dt.datetime = None
+    is_stopped: bool = True
 
-    @property
-    def end_time(self):
-        start_time = self.start_time if self.start_time is not None else dt.datetime.now()
-        return start_time + self.time
+    def get_end_time(self):
+        start_time = dt.datetime.now()
+        if self._count_from_time is not None and not self.is_stopped:
+            start_time = self._count_from_time
 
-    def is_started(self):
-        return self.start_time is not None
+        return start_time + self._time
 
     def get_time_left(self):
         current_time = dt.datetime.now()
-        time_left = self.end_time - current_time
+        time_left: dt.timedelta = self.get_end_time() - current_time
         return time_left if time_left.total_seconds() >= 0 else dt.timedelta()
 
     def get_seconds_left(self):
@@ -44,19 +44,65 @@ class Timer:
             weeks=weeks
         )
 
+        self._time = self.time
+
     def start(self):
+        self.is_stopped = False
+
         self.start_time = dt.datetime.now()
+        self._count_from_time = self.start_time
+    
+    def stop(self):
+        self.is_stopped = True
+
+        self._stop_time = dt.datetime.now()
+        self._time = self._time - (self._stop_time - self._count_from_time)
+
+    def resume(self):
+        self.is_stopped = False
+        self._count_from_time = dt.datetime.now()
 
     def reset(self):
+        self.is_stopped = True
         self.start_time = None
+        self._count_from_time = None
+        self._stop_time = None
+
 
 if __name__ == "__main__":
     from time import sleep
 
     timer = Timer()
-    timer.set_time(seconds=10)
+    timer.set_time(seconds=20)
     timer.start()
-    while timer.get_seconds_left() > 0:
-        print(timer.get_seconds_left())
-        sleep(2)
+    print("Start time:", timer.get_seconds_left())
 
+    sleep(2)
+    print("Timer before stop: ", timer.get_seconds_left())
+    timer.stop()
+    print("Timer stopped at: ", timer.get_seconds_left())
+
+    sleep(2)
+    print("Timer afer 2 seconds of stopped: ", timer.get_seconds_left())
+
+    timer.resume()
+    print("Timer after resumed", timer.get_seconds_left())
+
+    sleep(2)
+    print("Timer 2 seconds after resume", timer.get_seconds_left())
+
+    sleep(2)
+    print("Timer 2 seconds after resume", timer.get_seconds_left())
+
+    print("Timer before stop: ", timer.get_seconds_left())
+    timer.stop()
+    print("Timer stopped at: ", timer.get_seconds_left())
+
+    sleep(2)
+    print("Timer afer 2 seconds of stopped: ", timer.get_seconds_left())
+
+    timer.resume()
+    print("Timer after resumed", timer.get_seconds_left())
+
+    sleep(2)
+    print("Timer 2 seconds after resume", timer.get_seconds_left())
