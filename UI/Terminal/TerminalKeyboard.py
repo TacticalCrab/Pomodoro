32b600f27f9ea9f829475bcc3e6b2954aaf9c599
Piotr <pkaleksandrowicz@gmail.com>
2026-04-08 14:02:28 +0200

    feature: terminal keyboard key events handler

---
+++ UI/Terminal/TerminalKeyboard.py
@@ -0,0 +1,66 @@
+import msvcrt
+from threading import Thread
+
+
+class TerminalKeyboard:
+    _events: dict[str, set] = {}
+    _is_running = True
+    _thread = None
+
+    def __init__(self):
+        ...
+
+    def register_key_event(self, key, handler):
+        if key not in self._events:
+            self._events[key] = set()
+
+        self._events[key].add(handler)
+
+    def remove_key_event(self, key, handler):
+        if key not in self._events:
+            return
+
+        if handler not in self._events[key]:
+            return
+
+        self._events[handler].remove(handler)
+    
+    def clear_events(self):
+        self._events = {}
+    
+    def run(self):
+        while self._is_running:
+            key = msvcrt.getch()
+            if not self._is_running:
+                break
+            
+            if key == b'\x03':
+                raise KeyboardInterrupt
+
+            key = key.decode()
+            if key in self._events:
+                for handler in self._events[key]:
+                    handler()
+    
+    def start_thread(self):
+        self._thread = Thread(target=lambda: self.run())
+        self._thread.start()
+
+    def stop_thread(self):
+        if self._thread is not None:
+            self._is_running = False
+
+if __name__ == "__main__":
+    from time import sleep
+
+    tk = TerminalKeyboard()
+    tk.register_key_event("a", lambda: print("Yoooo!"))
+    tk.register_key_event("a", lambda: print("BAM!!"))
+    tk.register_key_event("c", lambda: tk.clear_events())
+    tk.register_key_event("b", lambda: tk.stop_thread())
+
+    tk.start_thread()
+
+    while True:
+        print("Hello world!")
+        sleep(1)
